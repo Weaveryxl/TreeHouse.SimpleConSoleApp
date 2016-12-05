677660e699bc67ff3040eed5958171fab1bd9acc
Weaveryxl <weaveryxl549@gmail.com>
2016-12-04 16:15:08 -0800

    first one?

---
+++ TreeHouse.SimpleConSoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeHouse.SimpleConSoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a comment
+            string greeting = "Hello World~";
+
+            string name = "Weaver YXL";
+
+            Console.Write(greeting + " ");
+            string[] a = SplitName(name);
+            Console.WriteLine(a[1]);
+        }
+
+        static string[] SplitName(string name)
+        {
+            return name.Split(' ');
+            
+        }
+    }
+}
